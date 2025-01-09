f185c9668d62a4dbeb21ccb504012540794c5f9d
Neil de Carteret <git@lumphammer.net>
2025-01-09 17:01:49 +0000

    825

--- src/module/NPCSheetClassV2.tsx
+++ src/module/NPCSheetClassV2.tsx
@@ -6,6 +6,8 @@ import { RecursivePartial } from "../types";
 import { InvestigatorActor } from "./InvestigatorActor";
 // import { reactTemplatePath, systemId } from "../constants";
 
+import DocumentSheetV2 = foundry.applications.api.DocumentSheetV2;
+
 const NPCSheet = React.lazy(() =>
   import("../components/characters/NPCSheet").then(({ NPCSheet }) => ({
     default: NPCSheet,
@@ -16,24 +18,20 @@ const NPCSheet = React.lazy(() =>
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
-class NPCSheetV2ClassBase extends foundry.applications.api
-  .DocumentSheetV2<InvestigatorActor> {
-  // @ts-expect-error yawn
-  static DEFAULT_OPTIONS: RecursivePartial<foundry.applications.types.ApplicationConfiguration> =
-    {
-      ...foundry.applications.api.ApplicationV2.DEFAULT_OPTIONS,
-      // classes: ["document-sheet"],
+class NPCSheetV2ClassBase extends DocumentSheetV2<InvestigatorActor> {
+  static DEFAULT_OPTIONS = {
+    ...foundry.applications.api.ApplicationV2.DEFAULT_OPTIONS,
 
-      window: {
-        ...foundry.applications.api.ApplicationV2.DEFAULT_OPTIONS.window,
-        frame: true,
-        title: "name",
-      },
-      position: {
-        height: 660,
-        width: 700,
-      },
-    };
+    window: {
+      ...foundry.applications.api.ApplicationV2.DEFAULT_OPTIONS.window,
+      frame: true,
+      title: "name",
+    },
+    position: {
+      height: 660,
+      width: 700,
+    },
+  };
 
   /** @override */
   // static get defaultOptions() {
--- src/module/PCSheetClassV2.tsx
+++ src/module/PCSheetClassV2.tsx
@@ -3,12 +3,12 @@ import { ReactApplicationV2Mixin } from "@lumphammer/shared-fvtt-bits/src/ReactA
 import { PCSheet } from "../components/characters/PCSheet";
 import { reactTemplatePath, systemId } from "../constants";
 
+import ActorSheetV2 = foundry.applications.sheets.ActorSheetV2;
 /**
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
-class PCSheetClassV2Base extends foundry.applications.sheets
-  .ActorSheetV2<Actor> {
+class PCSheetClassV2Base extends ActorSheetV2 {
   /** @override */
   static DEFAULT_OPTIONS = {
     ...foundry.applications.api.ApplicationV2.DEFAULT_OPTIONS,
